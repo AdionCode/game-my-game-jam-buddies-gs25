21f5c12866a3cdcfae7b49e9c5e635f9379cea5d
Rifqi Adi Mu'Ammar <adioncode@gmail.com>
2025-07-27 19:57:51 +0800

    New Minigames
    Are you a robot? minigames completed
    just need random to start

--- My Game Jam Buddy/Assets/Scripts/TaskManager.cs
+++ My Game Jam Buddy/Assets/Scripts/TaskManager.cs
@@ -1,27 +1,33 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 
 public class TaskManager : MonoBehaviour
 {
     [SerializeField] StudioProgress studio;
+    public GameObject minigamePanel;
+    private bool gameDone = false;
 
     [Header("Bug Hunter Task")]
-    public GameObject bugHunterPanel;
     public List<GameObject> bugButtons;
     public int bugToActivate = 3;
-
     private int bugsFixed = 0;
-    private bool gameDone = false;
+
+    [Header("Are You a Robot? Task")]
+    [SerializeField] GameObject capcthaObject;
+    [SerializeField] TextMeshProUGUI verifyText;
+    int left2press = 10;
 
     private void Start()
     {
-
+        StartCoroutine(AreYouARobot());
     }
 
     public void StartTask()
     {
         StartCoroutine(BugHunter());
+        StartCoroutine(AreYouARobot());
     }
     public bool IsTaskDone()
     {
@@ -31,7 +37,7 @@ public class TaskManager : MonoBehaviour
     #region Bug Hunter
     IEnumerator BugHunter()
     {
-        bugHunterPanel.SetActive(true);
+        minigamePanel.SetActive(true);
         bugsFixed = 0;
         gameDone = false;
 
@@ -64,7 +70,7 @@ public class TaskManager : MonoBehaviour
         // Cleanup
         foreach (var b in bugButtons)
             b.SetActive(false);
-        bugHunterPanel.SetActive(false);
+        minigamePanel.SetActive(false);
         gameDone = true;
     }
 
@@ -79,6 +85,44 @@ public class TaskManager : MonoBehaviour
             gameDone = true;
         }
     }
+    #endregion
+
+    #region Are You a Robot?
+
+    IEnumerator AreYouARobot()
+    {
+        gameDone = false;
+        minigamePanel.SetActive(true);
+        capcthaObject.SetActive(true);
+
+        left2press = Random.Range(10, 30);
+
+        float timeout = 10f;
+        float timer = 0f;
+
+        while (!gameDone && timer < timeout)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        capcthaObject.SetActive(false);
+        minigamePanel.SetActive(false);
+        gameDone = true;
+    }
+
+    public void VerifyButton()
+    {
+        verifyText.text = $"Yeah sure buddy.\r\nyou need to press {left2press} again.";
+        left2press--;
+
+        if (left2press == 0)
+        {
+            studio.AddXP(25);
+            gameDone = true;
+        }
+    }
+
     #endregion
 
 }
